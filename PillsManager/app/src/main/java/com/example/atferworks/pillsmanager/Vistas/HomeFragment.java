c3f4321465e0a3392a410b0700b84f33bb813950
Danny J. Garcia <djgar98@gmail.com>
2018-11-30 07:10:17 -0600

    Cambio de Icono

--- PillsManager/app/src/main/java/com/example/atferworks/pillsmanager/Vistas/HomeFragment.java
+++ PillsManager/app/src/main/java/com/example/atferworks/pillsmanager/Vistas/HomeFragment.java
@@ -3,6 +3,7 @@ package com.example.atferworks.pillsmanager.Vistas;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,7 @@ import com.example.atferworks.pillsmanager.R;
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+    View view;
 
 
     public HomeFragment() {
@@ -24,7 +26,10 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        RecyclerView reciclador = view.findViewById(R.id.recycler_home);
+
+        return view;
     }
 
 }
